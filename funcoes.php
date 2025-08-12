fe3e87aff2e4a323a4723b4757ee26ee7ebcad08
LuckR4y <137362549+LuckR4y@users.noreply.github.com>
2025-08-12 20:19:40 -0300

    Add files via upload

---
+++ funcoes.php
@@ -0,0 +1,13 @@
+<html>
+    <?php
+        function sum($x, $y){
+            $z = $x + $y;
+            return $z;
+        }
+
+        echo "5 + 10 = " . sum(5, 10) . "<br>";
+        echo "3 + 23 = " . sum(3, 23) . "<br>";
+        echo "8 + 35 = " . sum(8, 35) . "<br>";
+
+        ?>
+        </html>
